848517a23201c1556c24758583dbeb3c3513e901
khalix4 <jeremiah.abu@binghamuni.edu.ng>
2024-01-04 15:02:30 +0100

    added a form style

--- src/main/java/com/ng/edu/binghamuni/park/Domain/Form.java
+++ src/main/java/com/ng/edu/binghamuni/park/Domain/Form.java
@@ -1,4 +1,55 @@
-package main.java.com.ng.edu.binghamuni.park.Domain;
+package com.ng.edu.binghamuni.park.Domain;
 
 public class Form {
+    public class User {
+        private String username;
+        private String email;
+        private String password;
+
+        // Constructors
+        public User() {
+            // Default constructor
+        }
+
+        public User(String username, String email, String password) {
+            this.username = username;
+            this.email = email;
+            this.password = password;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        // toString method for debugging or logging
+        @Override
+        public String toString() {
+            return "User{" +
+                    "username='" + username + '\'' +
+                    ", email='" + email + '\'' +
+                    ", password='" + password + '\'' +
+                    '}';
+        }
+    }
+
 }
