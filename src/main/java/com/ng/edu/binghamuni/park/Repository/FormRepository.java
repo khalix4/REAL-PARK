2ef1d0a39268cd4177cc78bfcfde4f6c6f540a0a
Kelvin <kelvin.yashim@binghamuni.edu.ng>
2024-01-14 21:47:56 +0100

    Update FormRepository.java

--- src/main/java/com/ng/edu/binghamuni/park/Repository/FormRepository.java
+++ src/main/java/com/ng/edu/binghamuni/park/Repository/FormRepository.java
@@ -9,6 +9,5 @@ import java.util.List;
 @Repository
 public interface FormRepository extends JpaRepository <FormExample, Long> {
 
-    @Override
-    List<FormExample> findAll();
+    long countByAdSource(String adSource);
 }
